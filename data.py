7152a562d4b2869c71b8b5036c3bdd3e184d8892
Sierra17 <luissierramuntane17@gmail.com>
2019-06-09 13:08:01 +0200

    prueba 1

--- data.py
+++ data.py
@@ -1,7 +1,21 @@
 import pandas as pd
 import networkx as nx
-from pandas import DataFrame
+from pandas import Dataframe
+import staticmap
+from haversine import haversine
+from geopy.geocoders import Nominatim
 
-G = nx.Graph()
+url = 'https://api.bsmsa.eu/ext/api/bsm/gbfs/v2/en/station_information'
+bicing = DataFrame.from_records(pd.read_json(url)['data']['stations'], index='station_id')
+
+def main ():
+
+	G = nx.graph()
 
 
+
+
+
+
+
+main ()
